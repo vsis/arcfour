13b18af0eb013785821b1295e1b0c74f32051b8b
Roddy González <roddy.gonzalez.89@gmail.com>
2017-12-01 16:02:11 -0300

    Add some comments in test scripts

--- tests/test_random_key.sh
+++ tests/test_random_key.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# We should be capable to use a random 256 Bytes key
+
+# Get a random key
 head -c 256 < /dev/random > tests/rkey
+
+# Get a random file
 head -c 2048 < /dev/random > tests/2048.tmp
+
+# Cipher and decipher it, with our random key
 ./arcfour tests/rkey < tests/2048.tmp | ./arcfour tests/rkey > tests/2048.tmp.diff
+
+# Check difference
 diff tests/2048.tmp tests/2048.tmp.diff || exit 1
 
--- tests/test_wiki_examples.sh
+++ tests/test_wiki_examples.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# There are some examples in the RC4 article in Wikipedia.
+#So, let's check we get the same bytes
+
+# Cipher the example plain texts
 echo -n "Plaintext" | ./arcfour tests/Key > tests/plaintext.tmp
 echo -n "pedia" | ./arcfour tests/Wiki > tests/pedia.tmp
 echo -n "Attack at dawn" | ./arcfour tests/Secret > tests/attack.tmp
+
+# Check cipher texts against expected files
 diff tests/plaintext.{tmp,enc} || exit 1
 diff tests/pedia.{tmp,enc} || exit 1
 diff tests/attack.{tmp,enc} || exit 1
