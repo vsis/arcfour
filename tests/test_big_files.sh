36c552655166b79d73d0816e7585b129796b17b9
Roddy González <roddy.gonzalez.89@gmail.com>
2017-12-01 16:33:11 -0300

    Added a tests for big files

---
+++ tests/test_big_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This cipher is suposed to handle big files. Not simply test files.
+
+# Generate files of 1 MB, 10 MB and 100 MB
+head -c 1048576 < /dev/random > tests/1MB.tmp
+head -c 10485760 < /dev/random > tests/10MB.tmp
+head -c 104857600 < /dev/random > tests/100MB.tmp
+
+# Generate a 256 key
+head -c 256 < /dev/random > tests/key.tmp
+
+# Cipher and decipher
+./arcfour tests/key.tmp < tests/1MB.tmp | ./arcfour tests/key.tmp > tests/1MB.tmp.diff
+./arcfour tests/key.tmp < tests/10MB.tmp | ./arcfour tests/key.tmp > tests/10MB.tmp.diff
+./arcfour tests/key.tmp < tests/100MB.tmp | ./arcfour tests/key.tmp > tests/100MB.tmp.diff
+
+# Check difference
+diff tests/1MB.tmp tests/1MB.tmp.diff || exit 1
+diff tests/10MB.tmp tests/10MB.tmp.diff || exit 1
+diff tests/100MB.tmp tests/100MB.tmp.diff || exit 1
+
+# These are big files. So remove them.
+rm -r tests/1*MB.tmp*
